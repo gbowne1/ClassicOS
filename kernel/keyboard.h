a08648eff55a8d7d2e09c6268c968362af75f0a2
Gregory Bowne <gbowne144@gmail.com>
2026-01-24 22:21:44 -0800

    Update keyboard.h
    fixing missing includes and definition

--- kernel/keyboard.h
+++ kernel/keyboard.h
@@ -1,7 +1,10 @@
 #ifndef KEYBOARD_H
 #define KEYBOARD_H
 
+#include <stint.h>
+
 void keyboard_init(void);
+void keyboard_buffer_add(char c);
 char keyboard_get_char(void);  // Blocking read from buffer
 extern const char scancode_map[128];
 
