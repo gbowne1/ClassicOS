01f130227b9942987ff3ed060e6ddd004e99244c
Gregory Bowne <gbowne144@gmail.com>
2026-01-25 08:24:43 -0800

    Update keyboard.h
    fixed typo in header include

--- kernel/keyboard.h
+++ kernel/keyboard.h
@@ -1,11 +1,12 @@
 #ifndef KEYBOARD_H
 #define KEYBOARD_H
 
-#include <stint.h>
+#include <stdint.h>
 
 void keyboard_init(void);
 void keyboard_buffer_add(char c);
-char keyboard_get_char(void);  // Blocking read from buffer
-extern const char scancode_map[128];
+char keyboard_get_char(void);  
+
+extern const char scancode_map[128]; 
 
 #endif
