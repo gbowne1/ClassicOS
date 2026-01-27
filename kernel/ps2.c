a37f94de44462f791e9b1f2a778ece8b081cd3e0
Gregory Bowne <gbowne144@gmail.com>
2026-01-27 08:16:44 -0800

    Update ps2.c
    Fixed the `__volatile__` into a volatile keyword

--- kernel/ps2.c
+++ kernel/ps2.c
@@ -1,12 +1,12 @@
 #include "ps2.h"
 
 static inline void outb(uint16_t port, uint8_t val) {
-    __asm__ __volatile__ ("outb %b0, %w1" : : "a"(val), "Nd"(port));
+    __asm__ volatile ("outb %b0, %w1" : : "a"(val), "Nd"(port));
 }
 
 static inline uint8_t inb(uint16_t port) {
     uint8_t ret;
-    __asm__ __volatile__ ("inb %w1, %b0" : "=a"(ret) : "Nd"(port));
+    __asm__ volatile ("inb %w1, %b0" : "=a"(ret) : "Nd"(port));
     return ret;
 }
 
