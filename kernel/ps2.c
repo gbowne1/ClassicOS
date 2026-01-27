903061551c8586638ffded560d644b336cf86599
Gregory Bowne <gbowne144@gmail.com>
2026-01-27 12:41:17 -0800

    Update ps2.c
    Remove the definition of the in/out assembly and add io.h include instead of inline

--- kernel/ps2.c
+++ kernel/ps2.c
@@ -1,14 +1,5 @@
 #include "ps2.h"
-
-static inline void outb(uint16_t port, uint8_t val) {
-    __asm__ volatile ("outb %b0, %w1" : : "a"(val), "Nd"(port));
-}
-
-static inline uint8_t inb(uint16_t port) {
-    uint8_t ret;
-    __asm__ volatile ("inb %w1, %b0" : "=a"(ret) : "Nd"(port));
-    return ret;
-}
+#include "io.h"
 
 /* --- Controller Synchronization --- */
 
