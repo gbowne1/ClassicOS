cc92ade8fd3056a21d24fac76d376463e8f9b9c2
Gregory Bowne <gbowne144@gmail.com>
2026-01-26 17:06:44 -0800

    Update ps2.c
    Fixing asm to __asm__

--- kernel/ps2.c
+++ kernel/ps2.c
@@ -1,13 +1,12 @@
 #include "ps2.h"
 
-/* --- Low Level I/O Helpers --- */
 static inline void outb(uint16_t port, uint8_t val) {
-    asm volatile ("outb %0, %1" : : "a"(val), "Nd"(port));
+    __asm__ __volatile__ ("outb %b0, %w1" : : "a"(val), "Nd"(port));
 }
 
 static inline uint8_t inb(uint16_t port) {
     uint8_t ret;
-    asm volatile ("inb %1, %0" : "=a"(ret) : "Nd"(port));
+    __asm__ __volatile__ ("inb %w1, %b0" : "=a"(ret) : "Nd"(port));
     return ret;
 }
 
