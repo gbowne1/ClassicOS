45abf9418bcfc08d4675c1415bd784574c90dc7a
Borna Šoštarić <sostaric.borna.dev@hotmail.com>
2026-01-28 21:53:55 +0100

    fix formatting in klibc/string.h

--- klibc/include/string.h
+++ klibc/include/string.h
@@ -3,12 +3,12 @@
 
 #include <stddef.h>
 
-extern int memcmp(const void* s1, const void* s2, size_t n);
-extern void* memmove(void* dst, const void* src, size_t n);
-extern void* memcpy(void* dst, const void* src, size_t n);
-extern void* memset(void* dst, int c, size_t n);
+extern int memcmp(const void *s1, const void *s2, size_t n);
+extern void *memmove(void *dst, const void *src, size_t n);
+extern void *memcpy(void *dst, const void *src, size_t n);
+extern void *memset(void *dst, int c, size_t n);
 
-extern size_t strlen(const char* s);
-extern int strcmp(const char* s1, const char* s2);
+extern size_t strlen(const char *s);
+extern int strcmp(const char *s1, const char *s2);
 
 #endif  // CLASSICOS_KLIBC_STRING_H
